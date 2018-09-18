a009a9894473e15d5fa3ef694891bb6b9ce1df41
Mr0Dong <private0dong@gmail.com>
2018-09-18 21:53:09 +1000

    home page layout

--- SEP/src/main/java/com/example/developer/seplayout/HomeActivity.java
+++ SEP/src/main/java/com/example/developer/seplayout/HomeActivity.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
-class HomeActivity extends AppCompatActivity{
+public class HomeActivity extends AppCompatActivity{
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
